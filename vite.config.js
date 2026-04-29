e8d5d5afac0e750c18d7b2e304376c28574dff01
Nerdalerts <rawatadarsh20@gmail.com>
2026-04-30 01:28:00 +0530

    vite change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  // REPLACE 'your-repo-name' WITH YOUR ACTUAL GITHUB REPOSITORY NAME
+  base: '/your-repo-name/', 
   plugins: [
     react(),
     tailwindcss(),
