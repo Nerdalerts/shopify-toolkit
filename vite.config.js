a7e5e7e1529f00d40e447519b9b34a4efd4799ba
Nerdalerts <rawatadarsh20@gmail.com>
2026-04-30 01:50:24 +0530

    final change hopefully

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  // REPLACE 'your-repo-name' WITH YOUR ACTUAL GITHUB REPOSITORY NAME
-  base: 'https://github.com/Nerdalerts/shopify-toolkit', 
+  base: '/shopify-toolkit/', 
   plugins: [
     react(),
     tailwindcss(),
